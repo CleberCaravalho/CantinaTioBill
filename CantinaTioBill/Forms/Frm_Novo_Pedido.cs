c092d393c013136f4a472294aed2b61fe22ea76f
Marden Rocha <mardenbiel@gmail.com>
2021-02-07 15:30:35 -0300

    Implementando regras de negócio para um novo pedido.

--- CantinaTioBill/Forms/Frm_Novo_Pedido.cs
+++ CantinaTioBill/Forms/Frm_Novo_Pedido.cs
@@ -20,7 +20,7 @@ namespace CantinaTioBill.Forms
         public Frm_Novo_Pedido()
         {
             InitializeComponent();
-            
+
         }
 
         private void Btn_Procurar_Click(object sender, EventArgs e)
@@ -33,12 +33,12 @@ namespace CantinaTioBill.Forms
         {
             if (Txt_Cliente.Text != "")
             {
-                using(var context = new ApplicationDBContext())
+                using (var context = new ApplicationDBContext())
                 {
-                    _cliente = context.Clientes.Include(c=>c.Enderecos)
+                    _cliente = context.Clientes.Include(c => c.Enderecos)
                         .Where(c => c.Id == Convert.ToInt32(Txt_Cliente.Text)).First();
 
-                   
+
 
                     if (!_cliente.Equals(null))
                     {
@@ -46,11 +46,13 @@ namespace CantinaTioBill.Forms
                         Txt_Nome.Text = _cliente.Nome;
                         Txt_Endereco.Text = $"{_endereco.Rua}-{_endereco.Numero}-{_endereco.Bairro}";
 
-                        
+
 
                         var produtos = context.Produtos.ToList();
 
                         Dgv_Produtos.DataSource = produtos;
+                        CarregarColunas();
+
                         Btn_Adicionar_Produto.Enabled = true;
                         Btn_Excluir_Produto.Enabled = true;
                         Btn_Limpar_Produtos.Enabled = true;
@@ -66,7 +68,63 @@ namespace CantinaTioBill.Forms
 
         private void Btn_Adicionar_Produto_Click(object sender, EventArgs e)
         {
-            Dgv_Produtos_Adicionados.DataSource = Dgv_Produtos.SelectedRows[0].DataBoundItem;
+            DataGridViewRow coluna = new DataGridViewRow();
+
+            for (int i = 0; i < Dgv_Produtos.SelectedRows.Count; i++)
+            {
+                coluna = (DataGridViewRow)Dgv_Produtos.SelectedRows[i].Clone();
+                int intColIndex = 0;
+                foreach (DataGridViewCell celula in Dgv_Produtos.SelectedRows[i].Cells)
+                {
+                    coluna.Cells[intColIndex].Value = celula.Value;
+                    intColIndex++;
+                }
+                Dgv_Produtos_Adicionados.Rows.Add(coluna);
+            }
+            Dgv_Produtos_Adicionados.AllowUserToAddRows = false;
+            Dgv_Produtos_Adicionados.Refresh();
+
+            CalcularValorTotal();
+
+
+        }
+
+        private void CalcularValorTotal()
+        {
+
+             decimal soma = 0;
+             for (int i = 0; i < Dgv_Produtos_Adicionados.Rows.Count; ++i)
+             {
+                soma += (decimal)Dgv_Produtos_Adicionados.Rows[i].Cells[4].Value;
+             }
+
+            decimal desconto = Decimal.Parse("0.00");
+            if (Dgv_Produtos_Adicionados.Rows.Count >= 5)
+            {
+                desconto = Decimal.Multiply(soma, (decimal)0.215);
+                soma = soma-desconto;
+            }
+            Txt_Desconto.Text = desconto.ToString();
+            Txt_Valor.Text = soma.ToString();
+        }
+
+        private void CarregarColunas()
+        {
+            try
+            {
+                if (Dgv_Produtos_Adicionados.Columns.Count == 0)
+                {
+                    foreach (DataGridViewColumn dgvc in Dgv_Produtos.Columns)
+                    {
+                        Dgv_Produtos_Adicionados.Columns.Add(dgvc.Clone() as DataGridViewColumn);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao adicionar produtos selecionados");
+            }
         }
     }
 }
